3b554d12358bd391a15b0479a1dc5c77435abdb7
Captain-Nuc <99302853+Captain-Nuc@users.noreply.github.com>
2022-02-17 09:33:01 -0500

    Update master.ps1

--- master.ps1
+++ master.ps1
@@ -1,3 +1,3 @@
-Install-ADDSForest -DomainName "cdwtest.bro" -CreateDNSDelegation -DomainMode Win2012R2 -ForestMode Win2012R2 -DatabasePath "C:\Windows\NTDS" -SysvolPath "C:\Windows\SYSVOL" -LogPath "C:\Windows\NTDS"
+Install-ADDSForest -DomainName ${domainname} -CreateDNSDelegation -DomainMode Win2012R2 -ForestMode Win2012R2 -DatabasePath "C:\Windows\NTDS" -SysvolPath "C:\Windows\SYSVOL" -LogPath "C:\Windows\NTDS"
 
 Install-WindowsFeature -Name AD-Domain-Services,GPMC -computerName ContosoDC1 -IncludeManagementTools -Restart
